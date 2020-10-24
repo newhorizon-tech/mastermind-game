73f21e8c62e8c342e5a2e922a5d08c37255eba0a
newhorizon-tech <alaukikinbox@gmail.com>
2020-10-24 20:26:23 +0530

    Improve presentation

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-correct_combo = %w[1 2 3 4 5]
+correct_combo = %w[5 2 3 4]
 
 def num_input
   print 'Enter your guess: '
@@ -8,18 +8,27 @@ def num_input
   num.split('')
 end
 
-puts "Rules. \n 1. The number should have 5 digits. \n 2. You can't repeat digits \n"
-puts '-' * 40
+puts "Rules \n 1. The number should have 4 digits. \n 2. You can't repeat digits \n"
 
 win = false
-10.times do
-  turn = num_input
-  if turn.eql? correct_combo
-    win = true
-    puts 'You have lost the game!'
+correct_position = 1 # Correct digits at correct position
+incorrect_position = 0 # Correct digits at incorrect positon
+10.times do |turn|
+  puts
+  puts '-' * 40
+  puts "Turn no. #{turn + 1}"
+  puts '-' * 40
+  turn_guess = num_input
+  win = turn_guess.eql? correct_combo
+  if win
+    puts '-' * 40
+    puts 'You have won the game!'
     break
+  elsif correct_position.positive? or incorrect_position.positive?
+    puts "#{correct_position} correct digit(s) at correct position." if correct_position.positive?
+    puts "#{incorrect_position} correct digit(s) at incorrect position." if incorrect_position.positive?
   else
-    puts 'Incorrect'
+    puts 'No correct digit'
   end
 end
 
