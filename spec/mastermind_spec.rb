ee5ab27defcce1602b9582ce04eedf247748fc25
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-03 16:42:22 +0530

    Fix RSpec

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -5,7 +5,7 @@ describe Mastermind do
     it 'returns array with random numbers between 0 and 9' do
       Obj = Mastermind.new
       random_combo = Obj.random_combo_generator
-      test_condition = random_combo.all? { |x| (x < 5 and x > -1) }
+      test_condition = random_combo.all? { |x| (x < 10 and x > -1) }
       expect(test_condition).to eql(true)
     end
   end
