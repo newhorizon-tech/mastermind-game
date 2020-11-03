38a1e3daeecd8595b3f455a0b5c6e9ecefb9ee4f
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-03 16:34:13 +0530

    Add RSpec

---
+++ spec/mastermind_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/mastermind.rb'
+
+describe Mastermind do
+  describe 'Random num' do
+    it 'returns array with random numbers between 0 and 9' do
+      Obj = Mastermind.new
+      random_combo = Obj.random_combo_generator
+      test_condition = random_combo.all? { |x| (x < 10 and x > -1) }
+      expect(test_condition).to eql(true)
+    end
+  end
+end
